c5cf55a4eebc152628580c157a6e803f05ad9a98
Logan C <logan_cookman@hotmail.com>
2016-11-30 17:21:23 -0800

    I Commented out the unit test that is supposed to fail

--- unittestExample.py
+++ unittestExample.py
@@ -61,11 +61,13 @@ class TestListFunctions(unittest.TestCase):
 
         self.assertRaises(ValueError, self.theList.remove, -1)
 
+        """
     def test_thistestwillfail(self):
-        """show you what a failed test looks like
+        """#show you what a failed test looks like
         """
 
         # theList DOES contain 1.
         # remove will not raise the expected exception
 
         self.assertRaises(ValueError, self.theList.remove, 0)
+        """
